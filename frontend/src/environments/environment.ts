c841602068e33538914d22f0439e70f651aefa25
nadeem-github <m.nadeempatel@gmail.com>
2025-07-14 18:24:45 +0530

    remove main code

--- frontend/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    apiBaseUrl: 'http://localhost:8003/api/admin/',
-    imgBaseURL : 'http://localhost:8003/storage/images/',
-    // defaultSlide: './assets/images/defualtSlide.png',
-};
